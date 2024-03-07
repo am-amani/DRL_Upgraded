3c4dfb3efde75c59afe0498febceab8cc1764bac
Amir Mahdi Amani <33548412+am-amani@users.noreply.github.com>
2024-03-07 18:57:02 +0100

    Update distillation_test.py
    1. Fixing the actor object
    2. Adding model optimizer and scheduler

--- distillation_test.py
+++ distillation_test.py
@@ -3,8 +3,14 @@ import numpy as np
 from torchvision.transforms import Compose, Resize, ToTensor, Normalize
 from PIL import Image
 import cv2
-from distillation_CNNs import CNN_FeatureExtractor_Camera, CNN_FeatureExtractor_Scan, Encoder
+from distillation_CNNs import CNN_FeatureExtractor_Camera, CNN_FeatureExtractor_Scan, Encoder, Actor
 from replay_buffer import ReplayBuffer
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import os, copy
+from tensorboardX import SummaryWriter
+import datetime
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Device:", device)
@@ -53,16 +59,20 @@ def split_replay_buffer(replay_buffer, train_ratio=0.8):
 # Use the corrected function
 train_buffer, test_buffer, train_count, test_count = split_replay_buffer(replay_buffer)
 
-# Check sizes
-print("Expected size of the training data:", int(replay_buffer.size() * 0.8))
-print("Actual size of the training buffer:", train_buffer.size())
-print("Expected size of the testing data:", replay_buffer.size() - int(replay_buffer.size() * 0.8))
-print("Actual size of the testing buffer:", test_buffer.size())
+# # Check sizes
+# print("Expected size of the training data:", int(replay_buffer.size() * 0.8))
+# print("Actual size of the training buffer:", train_buffer.size())
+# print("Expected size of the testing data:", replay_buffer.size() - int(replay_buffer.size() * 0.8))
+# print("Actual size of the testing buffer:", test_buffer.size())
+
+# take random samples from the training buffer with batch size of 32 and unique samples for each batch
+# sample_batch_train = train_buffer.sample_batch(BATCH_SIZE)
+
+
 
 
 # show a sample batch from the replay buffer
-sample_batch_train = train_buffer.sample_batch(BATCH_SIZE)
-sample_batch_test = test_buffer.sample_batch(BATCH_SIZE)     
+#sample_batch_test = test_buffer.sample_batch(BATCH_SIZE)     
 
 
 #################PRINTING THE SAMPLE BATCH####################
@@ -122,44 +132,107 @@ transform_camera = Compose([
 #     Normalize(mean=[0.5], std=[0.5])
 # ])
 
-camera_data_batch = torch.stack([
-    transform_camera(Image.fromarray(camera_data.squeeze().astype(np.uint8))) for camera_data in sample_batch_train[5]
-])
+# camera_data_batch = torch.stack([
+#     transform_camera(Image.fromarray(camera_data.squeeze().astype(np.uint8))) for camera_data in sample_batch_train[5]
+# ])
 
-scan_data_batch = torch.stack([
-    torch.tensor(scan_data.reshape(1, -1), dtype=torch.float32) for scan_data in sample_batch_train[6]
-])
+# scan_data_batch = torch.stack([
+#     torch.tensor(scan_data.reshape(1, -1), dtype=torch.float32) for scan_data in sample_batch_train[6]
+# ])
 
-print("Scan data batch shape:", scan_data_batch.shape) #Scan data batch shape: torch.Size([32, 1, 360])
+# print("Scan data batch shape:", scan_data_batch.shape) #Scan data batch shape: torch.Size([32, 1, 360])
 
-print("Camera data batch shape:", camera_data_batch.shape) #Camera data batch shape: torch.Size([32, 1, 32, 64])
+# print("Camera data batch shape:", camera_data_batch.shape) #Camera data batch shape: torch.Size([32, 1, 32, 64])
 
 OUTPUT_DIM = 10
+LR = 1e-3
 
 # ASK MURAD ABOUT THE EVAL MODE OR TRAIN MODE
 model_camera = CNN_FeatureExtractor_Camera().eval().to(device)
+model_camera_optimizer = optim.Adam(model_camera.parameters(), lr=LR, weight_decay=1e-5)
+model_camera_scheduler = optim.lr_scheduler.StepLR(model_camera_optimizer, step_size=1000, gamma=0.1)
+
 model_scan = CNN_FeatureExtractor_Scan().eval().to(device)
+model_scan_optimizer = optim.Adam(model_scan.parameters(), lr=LR, weight_decay=1e-5)
+model_scan_scheduler = optim.lr_scheduler.StepLR(model_scan_optimizer, step_size=1000, gamma=0.1)
+
 model_encoder = Encoder(OUTPUT_DIM).eval().to(device)
+model_encoder_optimizer = optim.Adam(model_encoder.parameters(), lr=LR, weight_decay=1e-5)
+model_encoder_scheduler = optim.lr_scheduler.StepLR(model_encoder_optimizer, step_size=1000, gamma=0.1)
+
+model_actor = Actor(546, 2).eval().to(device)
+model_actor_optimizer = optim.Adam(model_actor.parameters(), lr=LR, weight_decay=1e-5)
+model_actor_scheduler = optim.lr_scheduler.StepLR(model_actor_optimizer, step_size=1000, gamma=0.1)
 
 
 # Define the train fucntion
-def train(model_camera, model_scan, model_encoder, data_batch):
+def train(data_batch, i):
     # create a temporary variable to increase +1 after each iteration.
     # This varible will be used to set set either camera data or scan data to the model. 
     # if it is even, then set the camera data to the model, otherwise set the scan data to the model.
-    temp = 0
-    if temp % 2 == 0:
-        print("Even Number - Camera data:", data_batch[5][i])
-        
-    else:
-        print("Odd Number - Scan data:", data_batch[6][i])
+    counter = i
+    #convert the state data to tensor and set it to the device
+    state_data_batch = torch.tensor(data_batch[0], dtype=torch.float32).to(device)
+    print("state_data_batch.shape:", state_data_batch.shape)
+    action_data_batch = torch.tensor(data_batch[1], dtype=torch.float32).to(device)
+    print("action_data_batch.shape:", action_data_batch.shape)
 
-    temp += 1
-        
+    if counter % 2 == 0: #if it is even, then set the camera data to the model
+        camera_data_batch = torch.stack([
+            transform_camera(Image.fromarray(camera_data.squeeze().astype(np.uint8))) for camera_data in data_batch[5]]).to(device)        
+        with torch.no_grad():
+            # Set the camera data to the model
+            features = model_camera(camera_data_batch)
+            print("features.shape:", features.shape)
+            #pass the features to the encoder
+            encoded_features = model_encoder(features)
+            print("encoded_features.shape:", encoded_features.shape)
+            #print the encoded features tensor
+            print(encoded_features)
+
+
+            concatenated_all_camera = torch.cat((features, encoded_features, state_data_batch), 1).to(device)
+            print("concatenated_features.shape:", concatenated_all_camera.shape)
+            print(concatenated_all_camera[0].dtype)
+
+            print(model_actor)
+            #print the features of the actor model
+            print(model_actor(concatenated_all_camera))
+
+
+    else: #if it is odd, then set the scan data to the model
+        scan_data_batch = torch.stack([
+            torch.tensor(scan_data.reshape(1, -1), dtype=torch.float32) for scan_data in data_batch[6]]).to(device)
+        with torch.no_grad():
+            # Set the scan data to the model
+            features = model_scan(scan_data_batch)
+            print("features.shape:", features.shape)
+            #pass the features to the encoder
+            encoded_features = model_encoder(features)
+            print("encoded_features.shape:", encoded_features.shape)
+            #print the encoded features tensor
+            print(encoded_features)
+
+
+            concatenated_all_scan = torch.cat((features, encoded_features, state_data_batch), 1).to(device)
+            print("concatenated_features.shape:", concatenated_all_scan.shape)
+            print(concatenated_all_scan[0].dtype)
+
+            print(model_actor)
+            #print the features of the actor model
+            print(model_actor(concatenated_all_scan))
+
+            # #calculate the loss usning F.mse_loss by comparing the output from the actor model and the action data batch
+            # loss = F.mse_loss(model_actor(concatenated_all_scan), action_data_batch)
+            # print("loss:", loss)
+
+            # # Optimize and step *****************************************
+            # loss.backward(retain_graph=True)
+
 
 
 # Define the evaluation function
-def evaluate(model, data_batch, criterion):
+def evaluate(data_batch, i):
     pass
 
 
@@ -173,13 +246,16 @@ for epoch in range(epochs):
     print("Progress: %d%%, Epoch: %d" % ((epoch/epochs)*100, epoch), end="\r") 
     ################## START: CALL THE TRAIN FUNCTION FOR THE MODEL ##################      
     for i in range(iter):
-        train(model_camera, model_scan, model_encoder, sample_batch_train)
+        sample_batch_train = train_buffer.sample_batch(BATCH_SIZE)
+        train(sample_batch_train, i)
     
     ################## END: CALL THE TRAIN FUNCTION FOR THE MODEL ####################
     
     ################## START: CALL THE EVALUATION FUNCTION FOR THE MODEL ##############
     if epoch > 0:
-        pass
+        sample_batch_test = test_buffer.sample_batch(BATCH_SIZE)
+        evaluate(sample_batch_test, i)     
+
         
     ################## END: CALL THE EVALUATION FUNCTION FOR THE MODEL ##################
     
